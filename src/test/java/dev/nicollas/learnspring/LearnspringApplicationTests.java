bcff490c3db4c208e012a014a644b22a7ae6d809
Nicollas <contatonicollasfeitosa@gmail.com>
2021-05-26 08:00:16 -0300

    initialize project

---
+++ src/test/java/dev/nicollas/learnspring/LearnspringApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.nicollas.learnspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

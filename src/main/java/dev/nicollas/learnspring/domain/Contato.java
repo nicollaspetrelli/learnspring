8249a7658f3c23d1417653d861327760276c7391
Nicollas <contatonicollasfeitosa@gmail.com>
2021-05-26 08:21:45 -0300

    Create model and H2 database configurations

---
+++ src/main/java/dev/nicollas/learnspring/domain/Contato.java
@@ -0,0 +1,67 @@
+package dev.nicollas.learnspring.domain;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "contatos")
+public class Contato {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nome;
+    private String email;
+    private String telefone;
+
+    public Contato(Integer id, String nome, String email, String telefone) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+    }
+
+    public Contato() {
+
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    @Override
+    public String toString() {
+        return "Contato{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", email='" + email + '\'' +
+                ", telefone='" + telefone + '\'' +
+                '}';
+    }
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+-- Comentado para criar a tabela via model, após a criação pode ser feita a inserção dos dados na tabela
+
+-- insert into contatos (nome, email, telefone) values
+--     ('Nicollas', 'nicollas@gmail.com', '99999-8899'),
+--     ('Jonh', 'john@icloud.com','88888-7777'),
+--     ('Does', 'does@icloud.com','66666-9999'),
+--     ('Mark', 'mark@icloud.com','22222-4444'),
+--     ('Uncle Bob', 'unclebob@icloud.com','33333-1111');
